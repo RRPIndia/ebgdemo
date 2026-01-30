ad894ddef4d0dadf0eb7e0cab2566e412c842389
sartaj <ssaarrttaajj111@gmail.com>
2026-01-30 15:27:04 +0530

    added sitemap, rrmoved unnecessary code from eleventy config

--- eleventy.config.mjs
+++ eleventy.config.mjs
@@ -1,14 +1,19 @@
-import eleventyNavigationPlugin from "@11ty/eleventy-navigation";
-
 export default function(eleventyConfig) {
   eleventyConfig.addPassthroughCopy("bundle.css");
   eleventyConfig.addPassthroughCopy("CNAME");
 
-  eleventyConfig.addPlugin(eleventyNavigationPlugin);
   eleventyConfig.addFilter("pathSegments", (url) => {
     return url
       .replace(/^\/|\/$/g, "")
       .split("/")
       .filter(Boolean);
   });	
-};
+
+  return {
+    dir: {
+      input: ".",
+      output: "_site"
+    }
+  };
+
+}
